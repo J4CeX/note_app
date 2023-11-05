130f0674d4ed629c626c2fcc8700f029f965baaa
Jacek <jacek_kozakowski_dev@wp.pl>
2023-11-05 10:31:06 +0100

    Version 2

---
+++ note_app.js
@@ -0,0 +1,46 @@
+import {notes} from './notes.js';
+
+const noteList = document.getElementById('list');
+
+const displayList = () => {
+  noteList.innerHTML = '';
+  notes.forEach((value) => {
+    const li = document.createElement('li');
+    li.setAttribute('id', value.id);
+    li.innerText = value.title;
+    noteList.appendChild(li);
+  });
+}
+
+displayList();
+
+const createButton = document.getElementById('create-button');
+
+createButton.addEventListener('click', () => {
+  const li = document.createElement('li');
+  const input = document.createElement('input');
+  const button = document.createElement('button');
+  input.setAttribute('type', 'text');
+  button.setAttribute('id', 'submit')
+  button.textContent = 'Submit';
+  li.appendChild(input);
+  li.appendChild(button);
+  noteList.appendChild(li);
+  noteList.app
+  document.querySelector('input').focus();
+
+  const submitButton = document.getElementById('submit');
+  submitButton.addEventListener('click', () => {
+    const inputNote = document.querySelector('input');
+    let newNote = {
+      id: notes[notes.length - 1].id + 1,
+      title: inputNote.value,
+      content: ''
+    }
+    notes.push(newNote);
+    displayList();
+  });
+});
+
+
+
---
+++ notes.js
@@ -0,0 +1,12 @@
+export let notes = [
+  {
+    id: 1,
+    title: 'Shop list',
+    content: '- butter\n- bread\n- ham'
+  },
+  {
+    id: 2,
+    title: 'Projects done',
+    content: '- calculator\n- quiz app\n- rock paper scissors\n- stop watch\n- tic tac toe\n- to do list'
+  }
+]
